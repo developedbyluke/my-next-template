7dae79545b6390812c6897e129d1e24e76a9cb36
developedbyluke <lukehorton7@yahoo.com>
2024-05-01 10:39:03 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tabWidth: 4,
+    semi: true,
+    trailingComma: "none",
+    plugins: ["prettier-plugin-tailwindcss"]
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-5xl font-semibold">Home</h1>
+            <p>Welcome to the home page!</p>
+            <Button variant="bordered">Click me</Button>
+        </div>
+    );
+}
